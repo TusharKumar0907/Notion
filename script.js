8ef9e18603161d11d0955a82f29535e6feee6378
TUSHAR <tktusharkumar12345@gmail.com>
2023-12-03 17:20:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+function changeImage(x) {
+
+var image = document.getElementById("imageeee");
+
+if(x == 1) {
+    
+    image.addEventListener('mouseover', function(){
+        image.src = "https://bit.ly/47USSLE"
+    });
+    
+    image.addEventListener('mouseout', function(){
+          image.src = "https://bit.ly/47USSLE"
+    });
+
+}
+
+if(x == 2) {
+
+    image.addEventListener('mouseover', function(){
+        image.src = "https://bit.ly/3T39QU0"
+    });
+    
+    image.addEventListener('mouseout', function(){
+          image.src = "https://bit.ly/47USSLE"
+    });
+
+}
+
+if(x == 3) {
+
+    image.addEventListener('mouseover', function(){
+        image.src = "https://bit.ly/414Ldbq"
+    });
+    
+    image.addEventListener('mouseout', function(){
+          image.src = "https://bit.ly/47USSLE"
+    });
+
+}
+
+if(x == 4) {
+
+    image.addEventListener('mouseover', function(){
+        image.src = "https://bit.ly/3uHmVIx"
+    });
+    
+    image.addEventListener('mouseout', function(){
+          image.src = "https://bit.ly/47USSLE"
+    });
+
+}
+
+
+}
+
+
+function changeImage2(x) {
+
+    var image = document.getElementById("imageeeeee");
+
+    if(x == 1) {
+        image.src = "https://bit.ly/3uHmVIx";
+    }
+
+    if(x == 2) {
+        image.src = " https://bit.ly/49XnSg5";
+    }
+
+    if(x == 3) {
+        image.src = "https://bit.ly/4a1jrAK";
+    }
+
+    if(x == 4) {
+        image.src = "https://bit.ly/4a3qybS";
+    }
+
+    if(x == 5) {
+        image.src = "https://bit.ly/46JOCxs";
+    }
+
+    if(x == 6) {
+        image.src = "https://bit.ly/3T39QU0";
+    }
+
+}
